6ce0c0a2e8e5d0bfdb13fd512f4f21c4d1303ad9
NweoWasTaken <121769486+NweoWasTaken@users.noreply.github.com>
2023-05-20 17:35:41 -0500

    Update Timer.ps1

--- Funny/Timer.ps1
+++ Funny/Timer.ps1
@@ -1,1 +1,1 @@
-$s=$m=$h=58;$e=":";while(1){cls;if($s-eq60){$s=0;$m++;if($m-eq60){$m=0;$h++}}$st="$s";if($s-lt10){$st="0"+$s}$mt="$m";if($m-lt10){$mt="0"+$m}$ht="$h";if($h-lt10){$ht="0"+$h}$t="$ht$e$mt$e$st";"`n $t";$s++;Start-Sleep -S 1}
+$s=$m=$h=0;$e=":";while(1){cls;if($s-eq60){$s=0;$m++;if($m-eq60){$m=0;$h++}}$st="$s";if($s-lt10){$st="0"+$s}$mt="$m";if($m-lt10){$mt="0"+$m}$ht="$h";if($h-lt10){$ht="0"+$h}$t="$ht$e$mt$e$st";"`n $t";$s++;Start-Sleep -S 1}
