eefdb42e0ad03defda974dc30ee9307f4cd6d095
NweoWasTaken <121769486+NweoWasTaken@users.noreply.github.com>
2023-05-20 18:24:24 -0500

    Update Timer.ps1
    Now works with only 185 characters!

--- Funny/Timer.ps1
+++ Funny/Timer.ps1
@@ -1,1 +1,1 @@
-$s=$m=$h=0;$e=":";for(1){cls;function x($v){if($v-lt10){return "0"+$v}return "$v"}if($s-eq60){$s=0;$m++;if($m-eq60){$m=0;$h++}}$a=x $s;$b=x $m;$c=x $h;$t="$c$e$b$e$a";"$t";$s++;Start-Sleep -S 1}
+$s=$m=$h=0;$e=":";for(1){cls;function x($v){if($v-lt10){return "0"+$v}return "$v"}if($s-gt59){$s=0;$m++}if($m-gt59){$m=0;$h++}$a=x $s;$b=x $m;$c=x $h;"$c$e$b$e$a";$s++;Start-Sleep -S 1}
